954d7e173f83aaac4b5f5aecac3b9858d7bef13e
jpedakal <jayakrishna490@gmail.com>
2026-01-01 00:01:11 +0530

    updated mongodb connection details

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -3,9 +3,17 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
     try {
         const URL = process.env.MONGO_DB_URL;
+        const options = {
+            useNewUrlParser: true, // Use the new URL string parser
+            useUnifiedTopology: true, // Use the new Server Discover and Monitoring engine
+            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+            maxPoolSize: 50,
+            minPoolSize: 10,
+            retryWrites: true, // Enable retryable writes
+        };
         console.log('URL', URL);
-        await mongoose.connect(URL);
-        console.log('✅ MongoDB Connected Successfully!');
+        await mongoose.connect(URL, options);
+        console.log('MongoDB Connected Successfully!');
     } catch (err) {
         console.log('Failed to connect Database');
     }
